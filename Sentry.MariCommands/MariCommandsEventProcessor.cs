aefcbc89dd048b98ddffd6f5b415f3a6249fbdee
renanrcp <renancamerapinheiro@gmail.com>
2020-08-17 16:37:12 -0300

    MariCommandsEventProcessor

---
+++ Sentry.MariCommands/MariCommandsEventProcessor.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Linq;
+using Microsoft.Extensions.Options;
+using Sentry.Extensibility;
+using Sentry.Extensions.Logging;
+using Sentry.Protocol;
+using OperatingSystem = Sentry.Protocol.OperatingSystem;
+
+namespace Sentry.MariCommands
+{
+    internal class MariCommandsEventProcessor : ISentryEventProcessor
+    {
+        private readonly SentryMariCommandsOptions _options;
+
+        public MariCommandsEventProcessor(IOptions<SentryMariCommandsOptions> options)
+            => _options = options?.Value;
+
+        public SentryEvent Process(SentryEvent @event)
+        {
+            // Move 'runtime' under key 'server-runtime' as User-Agent parsing done at
+            // Sentry will represent the client's, from SentryAspNetCore.
+            if (@event.Contexts.TryRemove(Runtime.Type, out var runtime))
+            {
+                @event.Contexts["server-runtime"] = runtime;
+            }
+
+            if (@event.Contexts.TryRemove(OperatingSystem.Type, out var os))
+            {
+                @event.Contexts["server-os"] = os;
+            }
+
+            // Not PII as this is running on a server
+            if (@event.ServerName == null)
+            {
+                // ServerName from the environment machine name is set by the Sentry base package.
+                // That is guarded by the SendDefaultPii since the SDK can be used in desktop apps.
+                // For ASP.NET Core apps, we always set the machine name if not explicitly set by the user, from SentryAspNetCore.
+                @event.ServerName = Environment.MachineName;
+            }
+
+            _options.ApplyDefaultTags(@event);
+
+            return @event;
+        }
+    }
+}
